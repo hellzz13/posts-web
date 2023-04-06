7134c7c9483c529b944f5a6244dd9786bb7fdc00
hellzz13 <hpalenca13@gmail.com>
2023-04-06 14:32:52 -0300

    initial setup

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{js,jsx,ts,tsx}"],
+    theme: {
+        extend: {
+            colors: {
+                primary: "#F24B59", //vermelho
+                secondary: "#325259", //cinza
+            },
+        },
+    },
+    plugins: [],
+};
